10f34f8bd86ada6db954b603e9cf51783f950271
wRiqie <henriquegame512@outlook.com>
2024-11-02 16:32:15 -0300

    index

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import http from "http";
+import path from "path";
 import { apiErrorHandler } from "./error/api_error_handler";
 import { datesRouter } from "./routes/dates_routes";
 import { matchRouter } from "./routes/match_routes";
@@ -15,6 +16,13 @@ const serverHttp = http.createServer(app);
 // Permitir CORS para todas as origens
 app.use(cors());
 
+// Serve arquivos estáticos da pasta 'public'
+app.use(express.static(__dirname));
+
+// Rota para a página inicial
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 app.use(express.json());
 app.use(matchRouter);
 app.use(teamsRouter);
